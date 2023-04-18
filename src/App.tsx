a89192bbc17a1d2d288d539a1f528a2781e6e78e
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-04-18 12:30:07 +0300

    feature: created getData function to fetch data from the API

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([]);
+  const [filteredSheet, setFilteredSheet] = useState(data[0]);
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  const getData = async () => {
+    try {
+      await fetch(
+        "https://www.wix.com/_serverless/hiring-task-spreadsheet-evaluator/sheets"
+      )
+        .then((res) => res.json())
+        .then((obj) => setData(obj.sheets));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(data);
+
+  return <div className="App"></div>;
 }
 
 export default App;
