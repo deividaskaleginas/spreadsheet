a38d0d0ddba95455cc0d25301b9d71c11d156055
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-04-23 13:21:24 +0300

    feature: created function to get data from API

---
+++ src/utils/getDataFromAPI.ts
@@ -0,0 +1,19 @@
+import { SheetData, SpreadSheetResponse } from "../types/dataTypes";
+
+export const getData = async (
+  setData: React.Dispatch<React.SetStateAction<SheetData[]>>,
+  setSubmissionUrl: React.Dispatch<React.SetStateAction<string>>
+) => {
+  try {
+    await fetch(
+      "https://www.wix.com/_serverless/hiring-task-spreadsheet-evaluator/sheets"
+    )
+      .then((res) => res.json())
+      .then((data: SpreadSheetResponse) => {
+        setData(data.sheets);
+        setSubmissionUrl(data.submissionUrl);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
